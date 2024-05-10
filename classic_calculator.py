ea6f563bb6f740c8dd9276308489d276346ce9fe
Kopi3 <kuba-kopacz@o2.pl>
2024-05-10 12:47:09 +0200

    changes in main.py

--- classic_calculator.py
+++ classic_calculator.py
@@ -3,131 +3,128 @@ import csv
 from dotenv import load_dotenv
 import tkinter as tk
 
-classic_calculator_history = []
+calculations = []
 
-def classic_calculator():
+load_dotenv()  
+name = os.getenv('IMIE')
 
-    load_dotenv()  
-    name = os.getenv('IMIE')
+root = tk.Tk()
+root.title(f'Welcome in calculator by {name}')
 
-    root = tk.Tk()
-    root.title(f'Welcome in calculator by {name}')
+def adding(x , y):
+    return(x + y)
 
-    def adding(x , y):
-        return(x + y)
+def subtraction(x , y):
+    return(x - y)
 
-    def subtraction(x , y):
-        return(x - y)
+def multiplication(x , y):
+    return(x * y)
 
-    def multiplication(x , y):
-        return(x * y)
+def division(x , y):
+    return(x / y)
 
-    def division(x , y):
-        return(x / y)
+def modulo(x , y):
+    return(x % y)
 
-    def modulo(x , y):
-        return(x % y)
+def power(x , y):
+    return(x**y)
 
-    def power(x , y):
-        return(x**y)
-
-    def add_digit(digit):
-        current_text = entry.get()    
-        new_text = current_text + digit
-        entry.delete(0, tk.END)
-        entry.insert(tk.END, new_text)
+def add_digit(digit):
+    current_text = entry.get()    
+    new_text = current_text + digit
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, new_text)
+    
+def clear():
+    entry.delete(0 , tk.END)
+    
+def add_dot():
+    current_text = entry.get()
+    if '.' not in current_text:
+        entry.insert(tk.END , '.')
         
-    def clear():
+def calculation_history():
+    history_window = tk.Toplevel(root)
+    history_window.title("Calculation history:")
+    history_text = tk.Text(history_window)
+    history_text.pack(expand=True, fill=tk.BOTH)
+    for calculation in calculations:
+        history_text.insert(tk.END, f"{calculation[0]} = {calculation[1]}\n")
+        
+def save_to_csv(calculations ,  file_name = 'Calculation history.csv'):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.join(current_directory , file_name)
+    with open(full_path , 'w') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for expression , result in calculations:
+            csv_writer.writerow([f"{expression}={result}"])
+
+entry = tk.Entry(root)
+entry.pack(side = tk.TOP , expand = True , fill = tk.BOTH)
+
+def results():
+    try:
+        expression = entry.get()
+        result = eval(expression)
         entry.delete(0 , tk.END)
+        entry.insert(tk.END , result)
+        calculations.append((expression, result))
+    except Exception as e:
+        entry.delete(0 , tk.END)
+        entry.insert(tk.END , f'Error: {e}')
+
+button = [
+        ('1','2','3'),
+        ('4','5','6'),
+        ('7','8','9'),
+        ('0'),
+        ('+','-'),
+        ('* ','/'),
+        ('modulo','  power'),
+        ('.','ac','='),
+        ('history',),
+        ('download history to csv.',)
+]
+
+for row in button:
+    button_frame = tk.Frame(root)
+    button_frame.pack(side = tk.TOP , expand = True , fill = tk.BOTH)
         
-    def add_dot():
-        current_text = entry.get()
-        if '.' not in current_text:
-            entry.insert(tk.END , '.')
+    for button_text in row:
+        
+        if button_text == ' ':
+            tk.Label(button_frame , text = '').pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
+        
+        elif button_text == '=':
+            btn = tk.Button(button_frame , text = button_text , command = results)
+            btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
+        
+        elif button_text == 'ac':
+            btn = tk.Button(button_frame, text=button_text, command=clear)
+            btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
             
-    def calculation_history():
-        history_window = tk.Toplevel(root)
-        history_window.title("Calculation history:")
-        history_text = tk.Text(history_window)
-        history_text.pack(expand=True, fill=tk.BOTH)
-        for calculation in classic_calculator_history:
-            history_text.insert(tk.END, f"{calculation[0]} = {calculation[1]}\n")
+        elif button_text == 'history':
+            btn = tk.Button(button_frame , text=button_text , command=calculation_history)
+            btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
             
-    def save_to_csv(classic_calculator_history ,  file_name = 'Calculation history.csv'):
-        current_directory = os.path.dirname(os.path.abspath(__file__))
-        full_path = os.path.join(current_directory , file_name)
-        with open(full_path , 'w') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            for expression , result in classic_calculator_history:
-                csv_writer.writerow([f"{expression}={result}"])
-
-    entry = tk.Entry(root)
-    entry.pack(side = tk.TOP , expand = True , fill = tk.BOTH)
-
-    def results():
-        try:
-            expression = entry.get()
-            result = eval(expression)
-            entry.delete(0 , tk.END)
-            entry.insert(tk.END , result)
-            classic_calculator_history.append((expression, result))
-        except Exception as e:
-            entry.delete(0 , tk.END)
-            entry.insert(tk.END , f'Error: {e}')
-
-    button = [
-            ('1','2','3'),
-            ('4','5','6'),
-            ('7','8','9'),
-            ('0'),
-            ('+','-'),
-            ('* ','/'),
-            ('modulo','  power'),
-            ('.','ac','='),
-            ('history',),
-            ('download history to csv.',)
-    ]
-
-    for row in button:
-        button_frame = tk.Frame(root)
-        button_frame.pack(side = tk.TOP , expand = True , fill = tk.BOTH)
+        elif button_text == 'download history to csv. on desktop':
+            btn = tk.Button(button_frame , text=button_text , command = lambda: save_to_csv(calculations))
+            btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
+                    
+        elif button_text in ('+','-' ,'* ','/'):
+            btn = tk.Button(button_frame , text = button_text , command = lambda op=button_text: add_digit(op))
+            btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
             
-        for button_text in row:
+        elif button_text in ('modulo'):
+            btn = tk.Button(button_frame , text = button_text , command = lambda op="%": add_digit(op))
+            btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
             
-            if button_text == ' ':
-                tk.Label(button_frame , text = '').pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
+        elif button_text in ('  power'):
+            btn = tk.Button(button_frame , text = button_text , command = lambda op="**": add_digit(op))
+            btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
+        
+        else:
+            btn = tk.Button(button_frame , text = button_text , command = lambda num=button_text: add_digit(num))
+            btn.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
             
-            elif button_text == '=':
-                btn = tk.Button(button_frame , text = button_text , command = results)
-                btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
-            
-            elif button_text == 'ac':
-                btn = tk.Button(button_frame, text=button_text, command=clear)
-                btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
-                
-            elif button_text == 'history':
-                btn = tk.Button(button_frame , text=button_text , command=calculation_history)
-                btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
-                
-            elif button_text == 'download history to csv. on desktop':
-                btn = tk.Button(button_frame , text=button_text , command = lambda: save_to_csv(classic_calculator_history))
-                btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
-                        
-            elif button_text in ('+','-' ,'* ','/'):
-                btn = tk.Button(button_frame , text = button_text , command = lambda op=button_text: add_digit(op))
-                btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
-                
-            elif button_text in ('modulo'):
-                btn = tk.Button(button_frame , text = button_text , command = lambda op="%": add_digit(op))
-                btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
-                
-            elif button_text in ('  power'):
-                btn = tk.Button(button_frame , text = button_text , command = lambda op="**": add_digit(op))
-                btn.pack(side = tk.LEFT , expand = True , fill = tk.BOTH)
-            
-            else:
-                btn = tk.Button(button_frame , text = button_text , command = lambda num=button_text: add_digit(num))
-                btn.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
-                
-    if __name__ == "__main__":
-        root.mainloop()
+root.mainloop()
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from dotenv import load_dotenv
-from enum import IntEnum
+from enum import Enum
 import os
 from classic_calculator import classic_calculator, classic_calculator_history
 from geometrical_calculator import geometrical_calculator, geometrical_calculator_history, calculation_history, save_to_csv
 
+class CalculatorChoice(Enum):
+    Classic = 1
+    Geometrical = 2
+    Show_history = 3
+    Save_to_csv = 4
+    exit = 5
+    
+def get_calculator_choice():
+    while True:
+        try:
+            choice = int(input("Number of the type of calculator you want to use: "))
+            if 1 <= choice <= 5:
+                return CalculatorChoice
+            else:
+                print("Invalid choice. Please enter a number between 1 and 5.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 def main_menu():
     print("Choose calculator:")
@@ -12,18 +29,19 @@ def main_menu():
     print("3. Show operations history")
     print("4. Save history of operations to csv. file")
     print("5. Exit")
-    choice = input("Number of the type of calculator you want to use: ")
 
-    if choice == "1":
+    calculator_choice = get_calculator_choice
+    
+    if calculator_choice == CalculatorChoice.Classic:
         classic_calculator()
-    elif choice == "2":
+    elif calculator_choice == CalculatorChoice.Geometrical:
         geometrical_calculator()
-    elif choice == "3":
+    elif calculator_choice == CalculatorChoice.Show_history:
         calculation_history()
-    elif choice == "4":
-        save_to_csv(classic_calculator_history, geometrical_calculator_history)
+    elif calculator_choice == CalculatorChoice.Save_to_csv:
+        save_to_csv()
     else:
-        print("There isn't such an operation, choose other one.")
+        print("Exiting...")
 
 if __name__ == "__main__":
     main_menu()
